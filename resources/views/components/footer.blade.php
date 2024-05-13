e420a889d751821dfdbdbc188450a7a8bf64a6f0
MDAMIAN <soner.12d@gmail.com>
2024-05-13 11:37:49 -0600

    INLINE ELEMENTOS LI

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -8,7 +8,7 @@
                 <p class="mb-4">Proporcionamos atención médica de calidad, accesible y conveniente. Nuestra misión es mejorar
                     la experiencia médica con citas flexibles y tecnología avanzada para el autogestionamiento de la
                     salud.</p>
-                
+
             </div>
         </div>
 
@@ -16,32 +16,34 @@
             <div class="md:w-1/2">
                 <div class="grid grid-cols-2 gap-x-4">
                     <ul class="list-none">
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">About us</a></li>
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Services</a></li>
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Blog</a></li>
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Contact us</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">About us</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Services</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Blog</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Contact us</a></li>
                     </ul>
                     <ul class="list-none">
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Support</a></li>
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Knowledge base</a></li>
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Live chat</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Support</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Knowledge base</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Live chat</a></li>
                     </ul>
                 </div>
             </div>
             <div class="md:w-1/2">
                 <div class="grid grid-cols-2 gap-x-4">
                     <ul class="list-none">
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Jobs</a></li>
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Our team</a></li>
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Leadership</a></li>
-                        <li><a href="#" class="text-gray-300 hover:text-blue-500">Privacy Policy</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Jobs</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Our team</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Leadership</a></li>
+                        <li class="inline"><a href="#" class="text-gray-300 hover:text-blue-500">Privacy Policy</a></li>
                     </ul>
                 </div>
             </div>
         </div>
 
         <div class="border-t border-gray-800 mt-8 pt-8 flex justify-between items-center">
-            <p class="text-center md:text-left mb-2 md:mb-0">&copy; <script>document.write(new Date().getFullYear())</script>. All Rights Reserved. </p>
+            <p class="text-center md:text-left mb-2 md:mb-0">&copy; <script>
+                    document.write(new Date().getFullYear())
+                </script>. All Rights Reserved. </p>
             <ul class="list-none flex items-center">
                 <li class="mr-4"><a href="#" class="text-gray-300 hover:text-blue-500">Terms &amp; Conditions</a></li>
                 <br>
